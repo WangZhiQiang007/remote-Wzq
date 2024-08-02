ac00a89ac20cf7a43be81b80d0ca3f121356f853
WangZhiQiang007 <163828347+WangZhiQiang007@users.noreply.github.com>
2024-08-02 15:11:58 +0800

    Delete BaiduNetdiskDownload directory

--- BaiduNetdiskDownload/10、黑马程序员瑞吉外卖平台实战开发/瑞吉外卖-资料/1 瑞吉外卖项目/代码/day01/reggie_take_out/src/main/java/zhiqiang/service/EmployeeService.java
+++
@@ -1,7 +0,0 @@
-package zhiqiang.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import zhiqiang.entity.Employee;
-
-public interface EmployeeService extends IService<Employee> {
-}
